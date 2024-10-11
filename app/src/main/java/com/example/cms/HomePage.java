283f6c6d228bcbf138f35869c9163572a9f7664d
Robert <robertgleison12@gmail.com>
2024-10-11 12:49:22 +0100

    New activity to home page

--- app/src/main/java/com/example/cms/HomePage.java
+++ app/src/main/java/com/example/cms/HomePage.java
@@ -1,6 +1,7 @@
 package com.example.cms;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,6 +19,7 @@ public class HomePage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home_page);
+        getWindow().getDecorView().setBackgroundColor(Color.WHITE);
 
         // Remove action window
         if (getSupportActionBar() != null) {
